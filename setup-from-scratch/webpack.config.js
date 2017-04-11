1e81ff03a7c9786beb93320556882cc2dc4adefa
Sangmin Yoon <sixmen@gmail.com>
2017-04-11 12:07:21 +0900

    Setup from scratch: add
    PR #1

---
+++ setup-from-scratch/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'app'),
+  entry: './index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css-loader')
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].css'
+    })
+  ]
+};
